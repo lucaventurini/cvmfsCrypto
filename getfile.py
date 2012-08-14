35b803814f35a038b6f98588e7e0ddd6645c4283
Luca Venturini <venturini.luc@gmail.com>
2012-08-14 18:53:18 +0200

    first commit

---
+++ getfile.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import os
+import urllib
+import binascii
+
+from base64 import b64encode, b64decode
+from M2Crypto.EVP import Cipher  
+from M2Crypto import SMIME, BIO
+
+ENC=1
+DEC=0
+"""
+
+# encrypt
+  cipher = EVP.Cipher(alg='aes_256_cbc', key=unhexlify(test['KEY']), iv=unhexlify(test['IV']), op=enc)
+  v = cipher.update(data)
+  v = v + cipher.final()
+  v = b64encode(v)
+  return v
+
+# decrypt
+data = b64decode(data)
+cipher = EVP.Cipher(alg='aes_256_cbc', key=unhexlify(test['KEY']), iv=unhexlify(test['IV']), op=dec)
+v = cipher.update(data)
+v = v + cipher.final()
+return v
+"""
+
+def main():
+    """ The script takes as argument the content of PATH_INFO, that is to say
+    the string after the script name in the query URL.
+    The format is: http://<server>/<path_to_the_script>/<file_id>~<key_id>
+    (note the "~" in between), where:
+    file_id: file to get
+    key_id: identifier of the key to encrypt the file
+    """
+
+    # SETTINGS for the key exchange server connection
+    KEY_SERVER_URL = "http://localhost/cgi-bin/"
+    KEY_SCRIPTNAME = "main.py/"
+
+    # SETTINGS for the server authentication
+    PRIVATE_KEY_FILENAME = "luventur_privkey.pem"
+    CERT_FILENAME = "luventurx509.cer"
+
+    print "Content-Type: text/plain\n"
+    
+    # Get the arguments
+    query = os.environ["PATH_INFO"][1:] # remove the first "/" from PATH_INFO
+    argv = query.split("~")
+    file_id = argv[0]
+    key_id = argv[1]
+
+    # Get the key
+    command = "get/"+key_id
+    pkcs7 = urllib.urlopen(KEY_SERVER_URL + KEY_SCRIPTNAME + command).read()
+
+    # Decript the key
+    s = SMIME.SMIME() # Prepare an SMIME object
+    def passphrase_fun(self):
+        return "pass"
+    s.load_key(PRIVATE_KEY_FILENAME, CERT_FILENAME, passphrase_fun)
+    p7 = SMIME.load_pkcs7_bio(BIO.MemoryBuffer(pkcs7))
+    key = s.decrypt(p7)
+
+    # Get the IV from the file id
+    iv = file_id[-33:-1] # Take 32 chars (128 bits) from the id, excluding the last (could be NaN)
+    iv = binascii.unhexlify(iv) # Translate to binary
+
+    # Get the file
+    clear_text = open(file_id).read() # TODO: open the real file from cvmfs
+
+    # Encrypt
+    cipher = Cipher('aes_256_cbc', key, iv, op=ENC) # TODO: ask the type of the key ('aes_256_cbc') to the server
+    v = cipher.update(clear_text)
+    v = v + cipher.final()
+    v = b64encode(v)
+
+    print v
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+
+import sys
+import redis
+import hashlib
+import os
+import cgi
+from M2Crypto import X509, SMIME, BIO, Rand
+
+class Commands:
+    """List of user commands"""
+    OK_MSG = "200 OK"
+    KO_MSG = "KO"
+
+    def get(self, sha1code):
+        """ Get key whose sha-1 is "sha1code", if exists """
+
+        key = rediserver.hget("sha1:"+sha1code, "key")
+        print key
+        return
+
+    def addk(self, gid):
+        """ Create a new random key for group "gid", and 
+        store it encrypted by public key of users in the group,
+        as PKCS7 file """
+
+        if self.__addk(gid): print self.OK_MSG
+        else: print self.KO_MSG
+        return
+
+    def __addk(self, gid):
+
+        # Prepare an SMIME object
+        s = SMIME.SMIME()
+        s.set_cipher(SMIME.Cipher('des_ede3_cbc'))
+
+        # Load all certificates from users in the group
+        cert_stack = X509.X509_Stack() 
+        users = rediserver.smembers("users_gid:"+gid)
+        if users:
+            for uid in users:
+                x509_string = rediserver.hget("x509_uid:"+uid, "x509")
+                x509 = X509.load_cert_string(x509_string)
+                if x509: cert_stack.push(x509)
+            s.set_x509_stack(cert_stack)
+
+            # Encrypt
+            # The new key is generated using secure random generator provided by OpenSSL
+            random_string = Rand.rand_bytes(16) # TODO: change the length of the key to support other block sizes
+            key = BIO.MemoryBuffer(random_string)
+            crypted_key = s.encrypt(key)
+
+            # Convert the key from BIO to string
+            # crypted_key.write(BIO.File(sys.stdout)) # Print to standard output
+            bio = BIO.MemoryBuffer()
+            crypted_key.write(bio)
+            pkcs7string = bio.read()
+
+            # Store the key in redis
+            sha1 = hashlib.sha1(random_string).hexdigest()
+            rediserver.sadd("keys_gid:"+gid, sha1)
+            rediserver.set("first_key_gid:"+gid, sha1)
+            rediserver.hmset("sha1:"+sha1, {"gid" : gid, "key_type" : "aes_256_cbc", "key" : pkcs7string})
+            # key_type is fixed to aes_256_cbc, for now        
+            return 1
+ 
+        return 0
+    
+    def remk(self, sha1code):
+        """ Remove the key whose sha-1 is "sha1code" """
+
+        if rediserver.exists("sha1:"+sha1code):
+            gid = rediserver.hget("sha1:"+sha1code, "gid")
+
+            # Remove the key from the group's list
+            rediserver.srem("keys_gid:"+gid, sha1code)
+
+            # Remove the key for good
+            rediserver.delete("sha1:"+sha1code)
+            print self.OK_MSG
+        print self.KO_MSG+": not existing key"
+        return
+    
+    def addu(self, x509_filename):
+        """ Add an user passing its x509 certificate """
+
+	# Let assume x509 certificate is in a local file, for now
+        x509 = X509.load_cert(x509_filename)
+        uid = x509.get_subject()
+        # TODO: verify certificate
+
+        if rediserver.sadd("uids", uid.as_text()):
+            x509_fp = open(x509_filename)
+            rediserver.hmset("x509_uid:"+uid.as_text(), {"x509" : x509_fp.read()})
+            x509_fp.close()
+            print self.OK_MSG
+        return
+
+    def remu(self, uid):
+        """ Remove an user by name """
+
+        # TODO: remove user from her groups
+        # NB: since the user's x509 is deleted, the keys are no more encrypted with her certificate,
+        # therefore, even if she is listed in the groups, she can no more decrypt new keys.
+        rediserver.srem("uids", uid)
+        rediserver.delete("x509_uid:"+uid)
+        print self.OK_MSG
+        return
+		
+    def join(self, uid, gid):
+        """Let user "uid" join group "gid" """
+
+        if rediserver.sismember("gids", gid) and rediserver.sismember("uids", uid):
+            if rediserver.sadd("users_gid:"+gid, uid) \
+            and self.__addk(gid): print self.OK_MSG
+        return
+    
+    def leave(self, uid, gid):
+	"""Let user "uid" leave group "gid" """
+
+        #TODO: remove old group key?       
+        if rediserver.srem("users_gid:"+gid, uid) and self.__addk(gid): print self.OK_MSG
+        else: print self.KO_MSG
+        return
+		
+    def addg(self, gid):
+	"""Create a new group"""
+
+	if rediserver.sadd("gids", gid) and self.__addk(gid):
+            print self.OK_MSG
+        else: print self.KO_MSG
+        return
+		
+    def remg(self, gid):
+	"""Delete a group and all keys it owns """
+
+        if rediserver.sismember("gids", gid):
+
+            # Delete keys
+            for sha1code in rediserver.smembers("keys_gid:"+gid):
+                self.remk(sha1code)
+            rediserver.delete("keys_gid:"+gid)
+
+            # Delete list of users
+            rediserver.delete("users_gid:"+gid)
+
+            # Delete gid from list of groups
+            rediserver.srem("gids", gid)
+            print self.OK_MSG
+
+        else: print self.KO_MSG+": "+gid+" doesn't exist."
+        return
+
+    def listg(self):
+        """ List groups """
+
+        print rediserver.smembers("gids")
+        return
+
+    def listu(self):
+        """ List users """
+
+        print rediserver.smembers("uids")
+        return
+
+    def listk(self, gid):
+        """ List all sha-1(key)s of group "gid" """
+
+        print rediserver.smembers("keys_gid:"+gid)
+        return
+
+    def getk(self, gid):
+        """ Get the sha-1 of the primary key for group "gid" """
+
+        print rediserver.get("first_key_gid:"+gid)
+        return
+		
+    def set_primary_key(self, gid, index):
+        return
+    
+
+def main():
+    if 'GATEWAY_INTERFACE' in os.environ:
+        print "Content-Type: text/plain\n"
+        query = "scriptname" + os.environ['PATH_INFO'] # e.g.: "scriptname/listk/atlas"
+        argv = query.split("/")
+    else:
+        argv = sys.argv
+
+    if len(argv) == 1:
+        print "Usage: ..."
+        return
+
+    # Start the connection to the redis server
+    global rediserver
+    rediserver = redis.StrictRedis(host='localhost', port=6379, db=0)
+	
+    # Read the command line and execute proper function
+    cmd = Commands()
+    getattr(cmd, argv[1])(*argv[2:])
+	
+    return
+
+if __name__ == '__main__':
+    main()
